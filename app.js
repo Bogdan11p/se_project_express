0944748d89e721de7300475b959b50bda4481fcd
Bogdan <bpintilie16@gmail.com>
2023-06-13 18:54:57 +0300

    Require fix

--- app.js
+++ app.js
@@ -7,16 +7,16 @@ mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db", (r) => {
   console.log("connected to DB", r);
 });
 
+app.use((req, res, next) => {
+  req.user = {
+    _id: "64858c4802d518caf1a4a541",
+  };
+  next();
+});
+
 const routes = require("./routes");
 app.use(express.json());
 app.use(routes);
-
-/* app.use((req, res, next) => {
-  req.user = {
-    _id: '64858c4802d518caf1a4a541'
-  };
-  next();
-}); */
 
 app.listen(PORT, () => {
   console.log(`Server is listening on ${PORT}`);

ef78b61dcfad8972ce29588b60cb15e66e89e403
Bogdan <bpintilie16@gmail.com>
2023-06-28 11:03:24 +0300

    Remvoed get user

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,12 @@
 const router = require("express").Router();
 
 const {
-  getUser,
   getUsers,
   getCurrentUser,
   updateCurrentUser,
 } = require("../controllers/users");
 const auth = require("../middlewares/auth");
-//router.get("/:userId", getUser);
+
 router.get("/me", auth.handleAuthError, getCurrentUser);
 router.patch("/me", auth.handleAuthError, updateCurrentUser);
 router.get("/", getUsers);
